d68a22854079e629c716431dfe388ab2f49ea9ee
KinooDev <eugetcheve@gmail.com>
2020-08-30 13:06:33 +0100

    Added a FilesManager.java for the data and basic files

---
+++ Core/src/main/java/com/kino/kstaffmode/managers/files/FilesManager.java
@@ -0,0 +1,41 @@
+package com.kino.kstaffmode.managers.files;
+
+import com.kino.kore.utils.files.BasicFilesManager;
+import com.kino.kore.utils.files.YMLFile;
+import com.kino.kstaffmode.KStaffMode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.configuration.file.FileConfiguration;
+
+@Getter
+public class FilesManager {
+
+    private BasicFilesManager basicFilesManager;
+    private YMLFile data;
+    private KStaffMode plugin;
+
+
+    public FilesManager (KStaffMode plugin) {
+        this.plugin = plugin;
+        this.basicFilesManager = new BasicFilesManager(plugin);
+        this.data = new YMLFile(plugin, "data");
+    }
+
+    public void start () {
+        basicFilesManager.registerConfig();
+        basicFilesManager.registerMessages();
+    }
+
+
+    public void saveData () {
+        data.save();
+    }
+
+    public FileConfiguration getMessages() {
+        return basicFilesManager.getMessages();
+    }
+
+    public void reloadConfig () {
+        plugin.reloadConfig();
+    }
+}
